0d81d4d946e6a3fc34825f1f1ce68026e9a5d86b
Brian Bradley <brianjbradley1@gmail.com>
2022-03-24 17:25:50 +0000

    Updated unit tests after contract upgrade

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -3,8 +3,9 @@ const ganache = require("ganache-cli");
 const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
-const compiledFactory = require("../ethereum/build/CampaignFactory.json");
-const compiledCampaign = require("../ethereum/build/Campaign.json");
+//const { abi, evm } = require('../compile');
+const { abi, evm } = require("../ethereum/build/CampaignFactory.json");
+const campaignContract = require("../ethereum/build/Campaign.json");
 
 let accounts;
 let factory;
@@ -15,8 +16,8 @@ beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
 
   // compiledFactory.interface = contract abi
-  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
-    .deploy({ data: compiledFactory.bytecode })
+  factory = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
     .send({ from: accounts[0], gas: "1000000" });
 
   await factory.methods.createCampaign("100").send({
@@ -24,12 +25,12 @@ beforeEach(async () => {
     gas: "1000000",
   });
 
-  // es6 syntax - destructuring array
-  // take first element out of array and assign to campaignAddress variable
+  //es6 syntax - destructuring array
+  //take first element out of array and assign to campaignAddress variable
   [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
 
   campaign = await new web3.eth.Contract(
-    JSON.parse(compiledCampaign.interface),
+    campaignContract.abi,
     campaignAddress
   );
 });
@@ -219,20 +220,30 @@ describe("Campaigns", () => {
 
     await campaign.methods.contribute().send({
       from: accounts[1],
-      value: web3.utils.toWei("10", "ether"),
+      value: web3.utils.toWei("5", "ether"),
     });
 
     await campaign.methods.contribute().send({
       from: accounts[2],
-      value: web3.utils.toWei("10", "ether"),
+      value: web3.utils.toWei("5", "ether"),
     });
+
+    // get campaign balance
+    const summary = await campaign.methods.getSummary().call();
+    const campaignBalance = web3.utils.fromWei(summary[1], "ether");
 
     await campaign.methods
       .createRequest("Buy tools", web3.utils.toWei("5", "ether"), manager)
       .send({ from: manager, gas: "1000000" });
 
+    // make sure 2 people have approved - i.e. > 50%
     await campaign.methods.approveRequest(0).send({
       from: accounts[1],
+      gas: "1000000",
+    });
+
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[2],
       gas: "1000000",
     });
 
