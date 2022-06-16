700ef8292af99ec3a0a4e958b3642ff9abe07dd9
Brian Bradley <brianjbradley1@gmail.com>
2022-06-16 19:13:10 +0100

    Added contractAddress function to automate adding this to factory.js after deploying

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,13 +1,14 @@
 import web3 from "./web3";
-const { abi } = require('./build/CampaignFactory.json');
-
+const { abi } = require("./build/CampaignFactory.json");
+const contractAddress = require("./constants/contractAddresses.json");
 
 //console.log(process.env.DEPLOYED_CONTRACT_ADDRESS);
 
+// console.log("Contract Address from file = " + typeof contractAddress[4]);
+// const addressHC = "0xcAD1F59e13Cd09340a12ce3C835C27243895C401";
+// console.log("Contract Address hardcoded  = " + typeof addressHC);
+
 // web3 provider config
-const instance = new web3.eth.Contract(
-  abi,
-  '0x2b2B2e28BcFFf2F772393DF8da46919eB1AC039C'
-);
+const instance = new web3.eth.Contract(abi, contractAddress[4].toString());
 
 export default instance;
