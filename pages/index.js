b116affd376a376d36430723062bcb116793a7c7
Brian Bradley <brianbradley55@hotmail.com>
2022-07-01 02:41:43 +0100

    Created initial project structure with moralis provider to connect wallet

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+// imports instead of require
+// nodejs != emcascript / javascript
+// backend js bit diff to front end js
+
+import Head from "next/head"
+import Header from "../components/Header"
+import styles from "../styles/Home.module.css"
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Crowdfund</title>
+                <meta name="description" content="Crowdfund Dapp" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Header />
+            <main className={styles.main}></main>
+            <footer className={styles.footer}></footer>
+        </div>
+    )
+}
