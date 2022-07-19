bd8578bbda92150343841ca22a6798d8331a4a15
Brian Bradley <brianbradley55@hotmail.com>
2022-07-19 16:44:31 +0100

    Desctuctured request object for readability

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -12,9 +12,8 @@ function RequestRow(props) {
     const router = useRouter()
 
     const { id, request, approversCount, address } = props
-    //const [ description, value, recipient, complete, approvalCount ] = request;
-
-    const readyToFinalize = request[4] > approversCount / 2
+    const { 0: description, 1: value, 2: recipient, 3: complete, 4: approvalCount } = request
+    const readyToFinalize = approvalCount > approversCount / 2
 
     const onApprove = async () => {
         setLoadingApproval(true)
@@ -82,32 +81,26 @@ function RequestRow(props) {
         }
     }
 
-    // Request[0] = Description
-    // Request[1] = Value
-    // Request[2] = Recipient
-    // Request[3] = Complete
-    // Request[4] = ApprovalCount
-
 
     return (
         <TableRow
             key={id}
             sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            disabled={request[3]}
-            positive={readyToFinalize && !request[3]}
+            disabled={description}
+            positive={readyToFinalize && !complete}
         >
             <TableCell>{id}</TableCell>
-            <TableCell>{request[0]}</TableCell>
-            <TableCell>{ethers.utils.formatUnits(request[1], "ether")}</TableCell>
-            <TableCell>{request[2]}</TableCell>
+            <TableCell>{description}</TableCell>
+            <TableCell>{ethers.utils.formatUnits(value, "ether")}</TableCell>
+            <TableCell>{recipient}</TableCell>
             <TableCell>
-                {request[4]}/{approversCount}
+                {approvalCount}/{approversCount}
             </TableCell>
             <TableCell>
-                {request[3] ? null : (
+                {complete ? null : (
                     <LoadingButton
                         loading={loadingApproval}
-                        disabled={readyToFinalize && !request[3]}
+                        disabled={readyToFinalize && !complete}
                         variant="contained"
                         onClick={onApprove}
                     >
@@ -116,11 +109,11 @@ function RequestRow(props) {
                 )}
             </TableCell>
             <TableCell>
-                {request[3] ? null : (
+                {complete ? null : (
                     <LoadingButton
                         loading={loadingFinalize}
                         variant="contained"
-                        disabled={!readyToFinalize && !request[3]}
+                        disabled={!readyToFinalize && !complete}
                         onClick={onFinalize}
                     >
                         Finalise
