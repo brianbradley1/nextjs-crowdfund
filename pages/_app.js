105860df3a9fd4bd5162c4262fbeb961ecb7d05d
Brian Bradley <brianbradley55@hotmail.com>
2022-07-20 14:48:51 +0100

    Updated config for nextjs and material ui integration

--- pages/_app.js
+++ pages/_app.js
@@ -1,43 +1,37 @@
-import { MoralisProvider } from "react-moralis"
-import { NotificationProvider } from "web3uikit"
-import "../styles/globals.css"
-
-//import PropTypes from "prop-types"
+import * as React from "react"
+import PropTypes from "prop-types"
 import Head from "next/head"
 import { ThemeProvider } from "@mui/material/styles"
 import CssBaseline from "@mui/material/CssBaseline"
 import { CacheProvider } from "@emotion/react"
-import theme from "./src/theme"
-import createEmotionCache from "./src/createEmotionCache"
+import theme from "../src/theme"
+import createEmotionCache from "../src/createEmotionCache"
+import { MoralisProvider } from "react-moralis"
 
+// Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
 
-function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }) {
+export default function MyApp(props) {
+    const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
     return (
         <CacheProvider value={emotionCache}>
             <Head>
                 <meta name="viewport" content="initial-scale=1, width=device-width" />
             </Head>
             <ThemeProvider theme={theme}>
-                {/* CssBaseline kickstart an elegant,
-				consistent, and simple baseline to
-				build upon. */}
-
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
                 <CssBaseline />
                 <MoralisProvider initializeOnMount={false}>
-                    <NotificationProvider>
-                        <Component {...pageProps} />
-                    </NotificationProvider>
+                    <Component {...pageProps} />
                 </MoralisProvider>
             </ThemeProvider>
         </CacheProvider>
     )
 }
 
-export default MyApp
-
-// MyApp.propTypes = {
-//     Component: PropTypes.elementType.isRequired,
-//     emotionCache: PropTypes.object,
-//     pageProps: PropTypes.object.isRequired,
-// }
+MyApp.propTypes = {
+    Component: PropTypes.elementType.isRequired,
+    emotionCache: PropTypes.object,
+    pageProps: PropTypes.object.isRequired,
+}
